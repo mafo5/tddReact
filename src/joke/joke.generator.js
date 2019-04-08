eb1f2d8f4689031a2db242b01a8710fb753b437d
Martin Fochler <mafo.work@googlemail.com>
2019-04-08 15:36:05 +0700

    implement response handling for loading a joke

--- src/joke/joke.generator.js
+++ src/joke/joke.generator.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import * as axios from 'axios';
+import Joke from './joke';
 
 export default class JokeGenerator extends React.Component {
     state = {
         joke: null,
         loading: false,
     }
+    _ismounted = false;
 
     loadJoke = async () => {
         this.setState({ loading: true });
+
+        const response = await axios.get('https://api.icndb.com/jokes/random');
+        if (!this._ismounted) {
+            return;
+        }
+
+        let joke = null;
+        if(response) {
+            const { data: { value } } = response;
+            joke = value.joke;
+        }
+        this.setState({ loading: false, joke });
     };
 
     render() {
@@ -17,11 +32,20 @@ export default class JokeGenerator extends React.Component {
             <React.Fragment>
                 {!joke && !loading && <div>You haven't loaded any joke yet!</div>}
                 {loading && <div>Loading...</div>}
+                {joke && !loading && <Joke text={joke}></Joke>}
 
                 <button onClick={this.loadJoke} type="button">
                     Load a random joke
                 </button>
             </React.Fragment>
         );
+    }
+
+    componentDidMount() { 
+        this._ismounted = true;
+    }
+    
+    componentWillUnmount() {
+        this._ismounted = false;
     }
 }
