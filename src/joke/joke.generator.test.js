612cad09ca0aef1e459b93eda6baac77a58b7efa
Martin Fochler <mafo.work@googlemail.com>
2019-04-08 12:51:38 +0700

    add second test for second feature

--- src/joke/joke.generator.test.js
+++ src/joke/joke.generator.test.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import JokeGenerator from './joke.generator';
-import { render } from 'react-testing-library';
+import { render, Simulate } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 
-test('JokeGenerator componente fetches a random joke and renders it', () => {
+test('JokeGenerator should show default message when no joke is loaded', () => {
     // renders JokeGenerator
     const { getByText } = render(
         <JokeGenerator></JokeGenerator>
     );
 
     expect(getByText('You haven\'t loaded any joke yet!')).toBeInTheDocument();
+});
+
+test('JokeGenerator componente fetches a random joke and renders it', () => {
+    // renders JokeGenerator
+    const { getByText, queryByText } = render(
+        <JokeGenerator></JokeGenerator>
+    );
+
+    Simulate.click(getByText('Load a random Joke'));
+
+    expect(queryByText('You haven\'t loaded any joke yet!')).not.toBeInTheDocument();
+    expect(queryByText('Loading ...')).toBeInTheDocument();
 });
