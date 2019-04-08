8490e1e0e4d1edf2641895019a336f37a2fac442
Martin Fochler <mafo.work@googlemail.com>
2019-04-08 12:38:11 +0700

    split test for each file

---
+++ src/joke/joke.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Joke from './joke';
+import { render } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+test('Joke componente receives props and then renders text', () => {
+    // renders Joke component with some text prop
+    const { getByTestId } = render(
+        <Joke text="The funniest joke this year" ></Joke>
+    );
+
+    expect(getByTestId('joke-text')).toHaveTextContent(
+        'The funniest joke this year'
+    );
+});
